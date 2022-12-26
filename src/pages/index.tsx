d2f13a4cbd7b50aaa3de124551d6517acb50d5fe
BAOKUNHAO\baocunhao <ngophongbao@gmail.com>
2022-12-26 18:31:38 +0700

    Update menu

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   };
 
   const toggleServiceMobile = () => {
-    setShowServiceMobile(!showServiceMobile);
+    if(showMenuMobile) setShowServiceMobile(!showServiceMobile);
   };
 
   return (
